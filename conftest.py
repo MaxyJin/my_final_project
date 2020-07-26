1738383646cb5fef04d107730b9916e89b54a4c6
Max <PhotoShot.98@mail.ru>
2020-07-26 13:34:14 +0300

    conftest.py language parameter adding commit

--- conftest.py
+++ conftest.py
@@ -1,5 +1,6 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default="chrome",
@@ -22,3 +23,17 @@ def browser(request):
     print("\nquit browser..")
     browser.quit()
 
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default='en', help='Chose language')
+
+
+@pytest.fixture(scope='function')
+def browser(request):
+    lang = request.config.getoption('language')
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': lang})
+    print('\nstart Chrome browser...')
+    browser = webdriver.Chrome(options=options)
+    yield browser
+    print('\nquit Chrome browser...')
+    browser.quit()
